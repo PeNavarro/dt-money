2117f4fa4fa15b1e578ab99d7b17315caecab14a
Pedro Mack Navarro <pedro@gerenteweb.com.br>
2024-01-10 11:57:47 -0300

    feat(transactions): create transactions context

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,7 +4,7 @@ import { SearchForm } from "../../components/SearchForm/index.tsx"
 import { Summary } from "../../components/Summary/index.tsx"
 import { TransactionsContainer, TransactionsTable, PriceHighlight } from "./styles.ts"
 import { TransactionsContext } from "../../contexts/TransactionsContext.tsx"
-
+import { dateFormatter, priceFormatter } from '../../utils/formatter.ts'
 export function Transactions(){
     const { transactions } = useContext(TransactionsContext)
 
@@ -24,11 +24,12 @@ export function Transactions(){
                                     <td width="50%">{transaction.description}</td>
                                     <td>
                                         <PriceHighlight variant={transaction.type}>
-                                            R$ {transaction.price}
+                                            {transaction.type === 'outcome' && '- '}
+                                            {priceFormatter.format(transaction.price)}
                                         </PriceHighlight>
                                     </td>
                                     <td>{transaction.category}</td>
-                                    <td>{transaction.createdAt}</td>
+                                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                                 </tr>
                             )
                         })}
